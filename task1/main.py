c5a85b7cb104f6f36f6e8b103517098b51828d50
r3v0h4n <larevoo@gmail.com>
2024-04-19 18:30:49 +0300

    task1

---
+++ task1/main.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        date_from = datetime.strptime(date, '%Y-%m-%d').date()
+    except ValueError as e:
+        print(e)
+        return 0
+    
+    date_to = datetime.today().date()
+    interval = date_to - date_from
+    return interval.days
+
+print(get_days_from_today('2022-10-09'))
